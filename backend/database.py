83b7df12ef923fa0844da1182e8e5737476766be
Juan Pablo Campo Tellez <jpcampo@cafam.com.co>
2025-10-20 21:53:39 -0500

    se hacen cambios para el deploy en railway

--- backend/database.py
+++ backend/database.py
@@ -5,7 +5,7 @@ from dotenv import load_dotenv
 
 load_dotenv()  # Carga el .env
 
-DATABASE_URL = os.getenv("DATABASE_URL")
+DATABASE_URL = "DATABASE_URL"
 
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
